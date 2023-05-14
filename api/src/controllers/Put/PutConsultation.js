0fe3054bdd4583cd25846765e07961aa1da0c031
Dylan <dylan.sebastianmte@gmail.com>
2023-05-14 20:40:34 -0300

    ddf

--- api/src/controllers/Put/PutConsultation.js
+++ api/src/controllers/Put/PutConsultation.js
@@ -6,10 +6,25 @@ const router = Router();
 // pedimos todos los usuarios
 router.put("/:id", async (req, res, next) => {
     try {
-       
-    } catch (error) {
+        const consultation = await Consulta.findByPk(req.params.id);
+        if (consultation) {
+            await Consulta.update({
+              dia: req.body.dia,
+              hora: req.body.hora,
+            });
         
-    }
-})
+            res.status(200).send(`Consulta actualizado ${JSON.stringify(Consulta)}`);
+           
+           
+          } else {
+            res.status(404).send("No se encontro la consulta el id especificado.");
+          }
+    
+       
+      } catch (error) {
+        next(error);
+        res.status(404);
+      }
+    });
 
 module.exports = router;
